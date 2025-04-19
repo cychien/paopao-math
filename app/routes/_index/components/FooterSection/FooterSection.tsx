fcdb42fb880b0f96e2003d11b9fba2ce6b8d80ef
cychien <xyz030206@gmail.com>
2025-04-19 22:08:53 +0800

    Update style and wording

--- app/routes/_index/components/FooterSection/FooterSection.tsx
+++ app/routes/_index/components/FooterSection/FooterSection.tsx
@@ -7,12 +7,12 @@ function FooterSection() {
         <img src={logoSrc} alt="Logo" className="w-12" />
         <p className="text-3xl font-semibold mt-7">數學考試交給寶哥</p>
         <p className="mt-4 text-text-tertiary text-xl">
-          超過 100 位高中生已報名，一起衝刺高分！
+          加入預售行列，開啟你的高分之路！
         </p>
 
         <div className="h-px mt-16 mb-8 bg-border-secondary w-full" />
 
-        <p className="text-text-quaternary font-inter">
+        <p className="text-text-quaternary font-inter text-center">
           © 2025 寶哥高中數學. All rights reserved.
         </p>
       </div>
